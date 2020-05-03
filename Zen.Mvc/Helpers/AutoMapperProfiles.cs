9dab3a079c01c79c1fd7b12097bbc08bfe868eff
Nezih Öztürk <nzhzturk@hotmail.com>
2020-05-03 18:39:50 +0200

    Add web mvc Add automapper

---
+++ Zen.Mvc/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zen.Mvc.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+
+    }
+}
---
+++ Zen.Mvc/Models/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zen.Mvc.Models
+{
+    public class ProductListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public  string CategoryName { get; set; }
+        public int  UnitInStock { get; set; }
+    }
+}
